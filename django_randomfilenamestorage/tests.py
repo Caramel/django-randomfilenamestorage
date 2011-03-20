8470a104e46aeae4fb96319da38e31cf29759c2a
Simon Law <SimonL@trustcentric.com>
2011-03-20 16:46:07 -0400

    Test creating a new RandomFilenameMetaStorage, with and without settings.

--- django_randomfilenamestorage/tests.py
+++ django_randomfilenamestorage/tests.py
@@ -1,14 +1,42 @@
+from __future__ import with_statement
+
+from contextlib import contextmanager
+
 import re
 
+from django.conf import settings
 from django.core.files.base import ContentFile
 from django.test import TestCase
+from django.utils.functional import LazyObject
 
 from django_randomfilenamestorage.storage import (
     RandomFilenameMetaStorage, RandomFilenameFileSystemStorage
 )
 
 
+class StubStorage(object):
+    def exists(*args, **kwargs):
+        return False
+
+
 class RandomFilenameTestCase(TestCase):
+    def assertFilename(self, name, length=16):
+        self.assertTrue(re.match(r'[0-9a-z]{%d}(?:\..+)?$' % length, name),
+                        '%r is invalid.' % name)
+
+    def test_init(self):
+        StorageClass = RandomFilenameMetaStorage(storage_class=StubStorage)
+        with patch(settings, RANDOM_FILENAME_LENGTH=NotImplemented):
+            storage = StorageClass()
+            self.assertFilename(storage.get_available_name(''))
+            storage = StorageClass(length=10)
+            self.assertFilename(storage.get_available_name(''), length=10)
+        with patch(settings, RANDOM_FILENAME_LENGTH=5):
+            storage = StorageClass()
+            self.assertFilename(storage.get_available_name(''), length=5)
+            storage = StorageClass(length=20)
+            self.assertFilename(storage.get_available_name(''), length=20)
+
     def test_get_available_name(self):
         length = 16
         storage = RandomFilenameFileSystemStorage(length=length)
@@ -43,3 +71,32 @@ class RandomFilenameTestCase(TestCase):
                                  name2),
                         '%r is invalid.' % name2)
         self.assertNotEqual(name1, name2)
+
+
+@contextmanager
+def patch(namespace, **values):
+    """Patches `namespace`.`name` with `value` for (name, value) in values"""
+    originals = {}
+    if isinstance(namespace, LazyObject):
+        if namespace._wrapped is None:
+            namespace._setup()
+        namespace = namespace._wrapped
+    for (name, value) in values.iteritems():
+        try:
+            originals[name] = getattr(namespace, name)
+        except AttributeError:
+            originals[name] = NotImplemented
+        if value is NotImplemented:
+            if originals[name] is not NotImplemented:
+                delattr(namespace, name)
+        else:
+            setattr(namespace, name, value)
+    try:
+        yield
+    finally:
+        for (name, original_value) in originals.iteritems():
+            if original_value is NotImplemented:
+                if values[name] is not NotImplemented:
+                    delattr(namespace, name)
+            else:
+                setattr(namespace, name, original_value)
