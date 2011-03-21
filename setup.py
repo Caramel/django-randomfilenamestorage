0d30ceb6b51ccf6a05df65343860ec89c3f02691
Simon Law <SimonL@trustcentric.com>
2011-03-20 22:32:43 -0400

    Better setup.py and documentation!

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='django-randomfilenamestorage',
     version='1.0',
     description=('A Django storage backend that gives random names to files.'),
+    long_description=open('README.rst', 'r').read(),
     author='Akoha Inc.',
     author_email='adminmail@akoha.com',
     url='http://bitbucket.org/akoha/django-randomfilenamestorage/',
-    packages=['django_randomfilenamestorage'],
-    package_dir={
-        'django_randomfilenamestorage': 'django_randomfilenamestorage'
-    },
+    packages=find_packages(),
+    install_requires=['Django>=1.0'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
